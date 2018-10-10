67fdc610fe6e5920b4b3badf52b74c5d4713d377
Owinges <owinges@gmail.com>
2018-10-10 21:10:58 +1100

    Modified API call to include pages and results-per-page. Changed to run the API calls simultaneously to increase speed.

--- test-two/src/index.js
+++ test-two/src/index.js
@@ -3,21 +3,35 @@ import { getUsersForOrganisation } from "./services/github";
 
 const baseUrl = "https://api.github.com/";
 const organisationId = "google";
-const gitHubApiAuthToken = ""; // add your GitHub API OAuth key here to increase request limit
+const gitHubApiAuthToken = "1f55a5198950f9517a7c35bc25ee124acde9831c";
+const pages = 22;
 
 export const go = async () => {
-  try {
+  let output = "";
+
+  const getPageOfUsers = async pageNumber => {
     const users = await getUsersForOrganisation(
       baseUrl,
       gitHubApiAuthToken,
-      organisationId
+      organisationId,
+      pageNumber
     );
 
-    const output = users.reduce((result, user) => {
+    output += users.reduce((result, user) => {
       return `${result}Username: ${user.login}\n`;
     }, "");
+  }
 
-    log(output);
+  let parallellCalls = [];
+
+  for (let i = 1; i <= pages; i++) {
+    parallellCalls.push(getPageOfUsers(i));
+  }
+
+  try {
+    await Promise.all(parallellCalls).then(() => {
+      log(output);
+    });
   } catch (error) {
     await log(error.message);
   }
--- test-two/src/services/github.js
+++ test-two/src/services/github.js
@@ -1,15 +1,16 @@
 import { get } from "../helpers/http";
 
 const getAuthenticationQueryString = async apiAuthenticationToken =>
-  `?access_token=${apiAuthenticationToken}`;
+  `access_token=${apiAuthenticationToken}`;
 
 export const getUsersForOrganisation = async (
   baseUrl,
   apiAuthenticationToken,
-  organisationId
+  organisationId,
+  pageNumber
 ) => {
   return get(
-    `${baseUrl}orgs/${organisationId}/members${await getAuthenticationQueryString(
+    `${baseUrl}orgs/${organisationId}/members?page=${pageNumber}&per_page=100&${await getAuthenticationQueryString(
       apiAuthenticationToken
     )}`
   );
