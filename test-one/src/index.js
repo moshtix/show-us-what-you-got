f72a49bb34698be9ff2625375b6439677286922d
FireMugen <maxxreeves@icloud.com>
2020-04-07 03:19:03 +1000

    Found and replaced multiples

--- test-one/src/index.js
+++ test-one/src/index.js
@@ -3,7 +3,23 @@ import { log } from "./helpers/logger";
 export const go = async () => {
   // Your code should go here instead of the welcome message below. Create and modify additional files as necessary/appropriate.
   const array = Array.from({length: 100}, (v, i) => i+1);
-  log(array);
+  
+  let result = ''
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+      result += 'BossHog ';
+    } else if (array[i] % 3 === 0) {
+      result += 'Boss ';
+    } else if (array[i] % 5 === 0) {
+      result += 'Hog ';
+    } else {
+      result += `${array[i]} `;
+    }
+  };
+
+  log(result);
+  
   const welcomeMessage =
     "Congrats, you're up and running! Welcome to the first moshtix code challenge! " +
     "Please replace this message with your solution and feel free to add " +
