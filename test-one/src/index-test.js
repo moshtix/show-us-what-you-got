f66fc1c4e1cf1185de9c67ddde9f1da98d33623d
Oliver Dunn <oged97@gmail.com>
2020-04-06 18:26:44 +1000

    added changes to the index.js and index-test.js to complete task one to print BossHog game for numbers 1 to 100. some rudimentary testing was added to the test file however im not well versed in this area so unsure if necessary or applicable in this instance.

--- test-one/src/index-test.js
+++ test-one/src/index-test.js
@@ -1,13 +1,55 @@
 import { go } from "./index";
 import * as logger from "./helpers/logger";
 
-test("logged welcome message", async () => {
-  const expectedOutput =
-    "Congrats, you're up and running! Welcome to the first moshtix code challenge! Please replace this message with your solution and feel free to add or modify other files as necessary. Good luck : )";
+test("number writer working", async () => {
 
   logger.log = jest.fn();
 
   await go();
 
-  expect(logger.log).toBeCalledWith(expectedOutput);
+  expect(logger.log).toHaveBeenCalledWith(1);
 });
+
+test("Boss writer working", async () => {
+
+  const Boss = "Boss";
+
+  logger.log = jest.fn();
+
+  await go();
+
+  expect(logger.log).toHaveBeenCalledWith(Boss);
+});
+
+test("Hog writer working", async () => {
+
+  const Hog = "Hog"
+
+  logger.log = jest.fn();
+
+  await go();
+
+  expect(logger.log).toHaveBeenCalledWith(Hog); 
+});
+
+test("BossHog writer working", async () => {
+
+  const BossHog = "BossHog"
+
+  logger.log = jest.fn();
+
+  await go();
+
+  expect(logger.log).toHaveBeenCalledWith(BossHog); 
+});
+
+//unsure if you want testing to be done however added as was part of the original script
+//also unsure fully how to implement testing as this is the first time i have encountered it
+
+//--------------------------------------------------------------------------------------//
+//also attempted to use the nthCalled to test specific values however the call order was// 
+//out of order and i was unsure what caused this.                                       //
+//for instance the 2nd call to logger.log was done by 8, instead of 2.                  //
+//this could be because of the testing done in logger-test but i am unsure as i dont    //
+// understnad the systems used in that file                                             // 
+//--------------------------------------------------------------------------------------//
--- test-one/src/index.js
+++ test-one/src/index.js
@@ -2,13 +2,21 @@ import { log } from "./helpers/logger";
 
 export const go = async () => {
   // Your code should go here instead of the welcome message below. Create and modify additional files as necessary/appropriate.
-
-  const welcomeMessage =
-    "Congrats, you're up and running! Welcome to the first moshtix code challenge! " +
-    "Please replace this message with your solution and feel free to add " +
-    "or modify other files as necessary. Good luck : )";
-
-  await log(welcomeMessage);
+  
+   
+    for(var i=1;i<=100;i++){
+      var output = "";    //generate a empty var
+      if(i%3==0){
+        output += "Boss"; //add Boss to output if true
+      }
+      if(i%5==0){
+        output += "Hog";  //add Hog to output if true
+      }
+      if(output == ""){   //adds i to output if output still empty
+        output = i;
+      }
+      await log(output); //output the value of output to the console using the predesigned function
+    }
 };
 
 go();
