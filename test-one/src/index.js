4d300956a67bd4288d211b6dd4f09b7329fecb90
InCahootsWith <elena.cabello@hotmail.com.au>
2020-04-06 19:23:23 +1000

    split into functions to allow scabaility

--- test-one/src/index.js
+++ test-one/src/index.js
@@ -1,11 +1,29 @@
 import { log } from "./helpers/logger";
 
-export const go = async () => {
+const isMultiple = (val, by) => {
+  return (val % by === 0)
+}
 
-  const result = (
-    // loop through and output result here
-    
-  )
+export const BossHog = (val) => {
+  switch (true) {
+    case isMultiple(val, 15):
+      return "BossHog";
+    case isMultiple(val, 3):
+      return "Boss";
+    case isMultiple(val, 5):
+      return "Hog";
+    default:
+      return val;
+  }
+}
+
+export const go = async (val) => {
+
+  let i;
+  let result = ""
+  for(i = 1; i <= 100; i++) {
+    result += BossHog(i) + " ";
+  }
 
   await log(result);
 };
