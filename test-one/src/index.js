e712bc9de5ff239a20935d999290ac818c6b1bfd
Ellie <elliechen.etc@gmail.com>
2020-04-06 16:26:46 +1000

    Test 1: Added basic solution

--- test-one/src/index.js
+++ test-one/src/index.js
@@ -1,7 +1,17 @@
 import { log } from "./helpers/logger";
 
 export const go = async () => {
-  // Your code should go here instead of the welcome message below. Create and modify additional files as necessary/appropriate.
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("BossHog");
+    } else if (i % 5 === 0) {
+      console.log("Hog");
+    } else if (i % 3 === 0) {
+      console.log("Boss");
+    } else {
+      console.log(i);
+    }
+  }
 
   const welcomeMessage =
     "Congrats, you're up and running! Welcome to the first moshtix code challenge! " +
