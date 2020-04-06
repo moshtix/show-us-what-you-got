b1eb9c1a4431b2d58ff90cf1194c4997f5e2da02
InCahootsWith <elena.cabello@hotmail.com.au>
2020-04-06 17:15:58 +1000

    initial commit of new branch

--- test-one/src/index-test.js
+++ test-one/src/index-test.js
@@ -1,10 +1,21 @@
 import { go } from "./index";
 import * as logger from "./helpers/logger";
 
-test("logged welcome message", async () => {
-  const expectedOutput =
-    "Congrats, you're up and running! Welcome to the first moshtix code challenge! Please replace this message with your solution and feel free to add or modify other files as necessary. Good luck : )";
+// test("logged welcome message", async () => {
+//   const expectedOutput =
+//     "Congrats, you're up and running! Welcome to the first moshtix code challenge! Please replace this message with your solution and feel free to add or modify other files as necessary. Good luck : )";
 
+//   logger.log = jest.fn();
+
+//   await go();
+
+//   expect(logger.log).toBeCalledWith(expectedOutput);
+// });
+
+// introduce new test here
+test("Prints Hog, Boss and HogBoss", async () => {
+  
+  const result = " "
   logger.log = jest.fn();
 
   await go();
--- test-one/src/index.js
+++ test-one/src/index.js
@@ -1,14 +1,13 @@
 import { log } from "./helpers/logger";
 
 export const go = async () => {
-  // Your code should go here instead of the welcome message below. Create and modify additional files as necessary/appropriate.
 
-  const welcomeMessage =
-    "Congrats, you're up and running! Welcome to the first moshtix code challenge! " +
-    "Please replace this message with your solution and feel free to add " +
-    "or modify other files as necessary. Good luck : )";
+  const result = (
+    // loop through and output result here
+    
+  )
 
-  await log(welcomeMessage);
+  await log(result);
 };
 
 go();
