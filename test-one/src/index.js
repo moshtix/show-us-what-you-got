82d482bd0707ce6218fc85b6eeb313619982f3b3
FireMugen <maxxreeves@icloud.com>
2020-04-07 03:07:57 +1000

    Created an array from 1-100 with array.from

--- test-one/src/index.js
+++ test-one/src/index.js
@@ -2,7 +2,8 @@ import { log } from "./helpers/logger";
 
 export const go = async () => {
   // Your code should go here instead of the welcome message below. Create and modify additional files as necessary/appropriate.
-
+  const array = Array.from({length: 100}, (v, i) => i+1);
+  log(array);
   const welcomeMessage =
     "Congrats, you're up and running! Welcome to the first moshtix code challenge! " +
     "Please replace this message with your solution and feel free to add " +
