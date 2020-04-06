75eb59e293bbeab1a27305150218601856c57ce7
InCahootsWith <elena.cabello@hotmail.com.au>
2020-04-06 19:25:21 +1000

    added comments about why 15 works

--- test-one/src/index.js
+++ test-one/src/index.js
@@ -6,7 +6,9 @@ const isMultiple = (val, by) => {
 
 export const BossHog = (val) => {
   switch (true) {
-    case isMultiple(val, 15):
+    case isMultiple(val, 15): 
+    // 15 is the first disviable number of 3 and 5 
+    // -> anything % 15 means it's / by 3 & 5
       return "BossHog";
     case isMultiple(val, 3):
       return "Boss";
